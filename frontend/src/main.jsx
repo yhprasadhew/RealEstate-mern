b312b903f4df93f21bb202d92d429451eb173b55
yhprasad <yhprasadhew@gmail.com>
2026-06-07 06:46:21 +0530

    main.jsx routing implemented

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+  <AuthProvider> 
+   
     <App />
+    </AuthProvider>
+
   </BrowserRouter>
-)
+);
