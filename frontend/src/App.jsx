a3b0dcea17528b109f686e44bfa37221112faca6
yhprasad <yhprasadhew@gmail.com>
2026-06-10 12:01:43 +0530

    resetPassword page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,20 +6,24 @@ import Register from "./pages/auth/Register";
 import VerifyEmail from "./pages/auth/VerifyEmail";
 import Login from "./pages/auth/login";
 import ForgotPassword from "./pages/auth/ForgotPassword";
+import ResetPassword from "./pages/auth/ResetPassword";
 
 const App = () => {
   return (
     <div>
       <Routes>
       
-         <Route path = "/login"  element={<Login/>} />
-         <Route path = "/verify-email" element={<VerifyEmail/>} />
+        <Route path = "/login"  element={<Login/>} />
+        <Route path = "/verify-email" element={<VerifyEmail/>} />
         <Route path = "/register" element={<Register/>} />
+        <Route path ="/forgot-password" element={<ForgotPassword /> } />
+        <Route path ="/reset-password" element={<ResetPassword /> } />
+
+
         <Route path="/" element={<LandingPage />} />
         <Route path ="/properties" element={<Properties/> } />
          <Route path ="/properties/:id" element={<PropertyDetails/> } />
-          <Route path ="/forgot-password" element={<ForgotPassword /> } />
-         ForgotPassword 
+ 
 
       </Routes>
     </div>
